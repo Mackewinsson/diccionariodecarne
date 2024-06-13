e05981fbfa5bdd334ad0b7461d8b8cfb13fefb54
Mackewinsson P <mackewinsson@gmail.com>
2024-06-13 19:43:48 +0200

    start changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-import createUser from "../actions/users/createUser";
-const { default: connectDB } = require("../app/lib/connectDB");
-
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
