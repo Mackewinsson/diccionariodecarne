a5ef7e995c3d5f756079f9b3a82d0ec9af219079
gervislm <gervislm@icloud.com>
2024-06-15 18:50:11 -0400

    interface changes: db + styles

---
+++ src/db/db.js
@@ -0,0 +1,37 @@
+export const meatCuts = {
+  Venezuela: {
+    "Bife de Chorizo": "Bife de Chorizo",
+    Entraña: "Entraña",
+    "Asado de Tira": "Asado de Tira",
+    Lomo: "Lomo",
+    Solomo: "Ribeye",
+  },
+  Argentina: {
+    "Bife de Chorizo": "Bife de Chorizo",
+    Entraña: "Entraña",
+    "Asado de Tira": "Asado de Tira",
+    Lomo: "Lomo",
+    Ribeye: "Bife Ancho",
+  },
+  Chile: {
+    "Bife de Chorizo": "Bife de Chorizo",
+    Entraña: "Entraña",
+    "Asado de Tira": "Asado de Tira",
+    Lomo: "Lomo",
+    Ribeye: "Lomo Vetado",
+  },
+  España: {
+    "Bife de Chorizo": "Entrecot",
+    Entraña: "Entraña",
+    "Asado de Tira": "Costillas de Ternera",
+    Lomo: "Solomillo",
+    Ribeye: "Chuletón",
+  },
+  USA: {
+    "Bife de Chorizo": "Sirloin",
+    Entraña: "Skirt Steak",
+    "Asado de Tira": "Short Ribs",
+    Lomo: "Tenderloin",
+    Ribeye: "Ribeye",
+  },
+};
